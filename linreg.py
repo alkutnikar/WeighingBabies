4b68df6285734559928fb166cd39fa9865233025
Ajay Lakshminarayanarao <alkutnikar@gmail.com>
2014-11-16 20:08:02 -0500

    Single Command File

---
+++ linreg.py
@@ -0,0 +1,193 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+babiesDF = pd.read_csv('/home/rakesh/Desktop/Data Science/Data/2008_birth_data.csv')
+slicedDF = pd.DataFrame
+trainDF = pd.DataFrame
+testDF = pd.DataFrame
+evalDF = pd.DataFrame
+
+colIndex = 0
+def binarizeColumn(x):
+    global colIndex
+    if x == colIndex:
+        return 1
+    else:
+        return 0
+
+def binarizeHisp(x):
+    if x == 'N':
+        return 0
+    else:
+        return 1
+
+#global colIndex
+#global slicedDF
+requiredColumns = ['SEX', 'MARITAL','FAGE', 'MAGE', 'FEDUC', 'MEDUC', 'TOTALP', 'BDEAD', 'TERMS', 'LOUTCOME', 'WEEKS', 'BPOUND', 'BOUNCE', 'WEIGHT', 'RACEMOM', 'RACEDAD', 'HISPMOM', 'HISPDAD', 'CIGNUM', 'DRINKNUM', 'GAINED', 'ANEMIA', 'CARDIAC', 'ACLUNG', 'DIABETES', 'HERPES', 'HYDRAM', 'HEMOGLOB', 'HYPERCH', 'HYPERPR', 'ECLAMP',
+                             'CERVIX','PINFANT','PRETERM','RENAL','RHSEN','UTERINE']
+slicedDF = babiesDF.ix[:,requiredColumns]
+
+print "Number of rows before removal are", len(slicedDF)
+
+#Removing rows with missing weight details
+slicedDF = slicedDF[slicedDF['BPOUND']!=99]
+slicedDF = slicedDF[slicedDF['BOUNCE']!=99]
+len(slicedDF)
+
+#adding a new column for weight in pounds
+weightPound = slicedDF['BPOUND']
+weightOunces = slicedDF['BOUNCE']
+weight = weightPound.astype(np.float) + (0.0625 * weightOunces.astype(np.float))
+slicedDF['WEIGHTLB'] = weight
+raceColumns = ['OTHER_NON_WHITE', 'WHITE', 'BLACK', 'AMERICAN_INDIAN', 'CHINESE', 'JAPANESE', 'HAWAIIAN', 'FILIPINO', 'OTHER_ASIAN']
+for race in raceColumns:
+    slicedDF[race + '_MOM'] = slicedDF['RACEMOM'].map(binarizeColumn)
+    colIndex = colIndex + 1
+colIndex = 0
+for race in raceColumns:
+    slicedDF[race + '_DAD'] = slicedDF['RACEMOM'].map(binarizeColumn)
+    colIndex = colIndex + 1
+slicedDF['HISPMOM_BINARY'] = slicedDF['HISPMOM'].map(binarizeHisp)
+slicedDF['HISPDAD_BINARY'] = slicedDF['HISPDAD'].map(binarizeHisp)
+columnsToDrop = ['BPOUND','BOUNCE','RACEMOM','RACEDAD','HISPMOM','HISPDAD']
+for column in columnsToDrop:
+    slicedDF = slicedDF.drop(column, 1)
+
+missing99Columns = ['MAGE','FEDUC','MEDUC','TOTALP','BDEAD','TERMS','WEEKS','WEIGHT','CIGNUM','DRINKNUM','GAINED']
+missing9Columns = ['LOUTCOME','ANEMIA','CARDIAC','ACLUNG','DIABETES','HERPES','HYDRAM','HEMOGLOB','HYPERCH','HYPERPR','ECLAMP'
+                   ,'CERVIX','PINFANT','PRETERM','RENAL','RHSEN','UTERINE']
+missingValDF = slicedDF[slicedDF['FAGE']!=99]
+
+for col in missing99Columns:
+    missingValDF = missingValDF[missingValDF[col]!=99]
+    
+for col in missing9Columns:
+    missingValDF = missingValDF[missingValDF[col]!=9]
+
+print "Number of rows after removing missing values:",len(missingValDF)
+
+def get_splitDF():
+        global missingValDF
+        global trainDF
+        global testDF
+        global evalDF
+        DF_temp = pd.DataFrame
+        rand_nos = np.random.rand(len(missingValDF)) < 0.7
+        trainDF = missingValDF[rand_nos]
+        DF_temp = missingValDF[~rand_nos]
+
+        rand_nos = np.random.rand(len(DF_temp)) < 0.6
+        testDF = DF_temp[rand_nos]
+        evalDF = DF_temp[~rand_nos]
+
+        print 'Train(len) : {0} rows'.format(str(len(trainDF)))
+        print 'Test(len) : {0} rows'.format(str(len(testDF)))
+        print 'Eval(len) : {0} rows'.format(str(len(evalDF)))
+
+get_splitDF()
+
+trainWeights = trainDF['WEIGHTLB']
+testWeights = testDF['WEIGHTLB']
+evalWeights = evalDF['WEIGHTLB']
+trainDF = trainDF.drop(['WEIGHTLB','WEIGHT'], 1)
+testDF = testDF.drop(['WEIGHTLB','WEIGHT'], 1)
+evalDF = evalDF.drop(['WEIGHTLB','WEIGHT'], 1)
+
+#Converting our final dataframe into a n-dimensional list. 
+#This is required for passing our data to any interpolation model.
+#print trainDF.columns[30]
+allRowsList = []
+for idx,row in trainDF.iterrows():
+    currentRowList = row.values.tolist()
+    allRowsList.append(currentRowList)
+
+def runRegression(type, passedDf, passedWeights):
+	newTestDF = pd.DataFrame()
+	newTestDF = passedDf.copy()
+	from sklearn import linear_model
+	if type == 0:
+		clf = linear_model.LinearRegression()
+	elif type == 1:
+		clf = linear_model.Ridge(alpha=0.5)
+	clf.fit(allRowsList, trainWeights)
+	coeffArray = clf.coef_
+	intercept = clf.intercept_ 
+	print "Co-efficients of linear regression are", coeffArray
+	print "Intercept is",intercept
+
+	testWeightsList = passedWeights.tolist()
+	i = 0
+	olsPrediction = []
+	for idx,row in newTestDF.iterrows():
+	    currentRowList = row.values.tolist()
+	    predictedWeightRegression = np.dot(coeffArray,currentRowList) + intercept
+	    olsPrediction.append(predictedWeightRegression)
+	    #print "Predicted Weight is", predictedWeightRegression
+	    actualWeight = testWeightsList[i]
+	    #print "Actual Weight is", actualWeight
+	    i = i + 1
+
+	sammyRow = [2, 1, 27, 27, 12, 16, 1, 0, 0, 1, 40, 0, 0.25, 15, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0]
+
+	if len(passedDf) < 10000:
+		if type == 0:
+			print "Prediction for Sammy's child according to OLS Regression is", np.dot(coeffArray,sammyRow) + intercept
+		else:
+			print "Prediction for Sammy's child according to Ridge Regression is", np.dot(coeffArray,sammyRow) + intercept
+	if type == 0:
+		newTestDF['PREDICTED_WEIGHT_OLS'] = olsPrediction
+	else:
+		newTestDF['PREDICTED_WEIGHT_RIDGE'] = olsPrediction
+	newTestDF['ACTUAL_WEIGHT'] = passedWeights
+	newTestDF['BASELINE_WEIGHT'] = np.mean(testWeightsList)
+
+	import math
+	newTestDF['BASELINE_SQUARED_ERROR'] = (newTestDF['BASELINE_WEIGHT'] - newTestDF['ACTUAL_WEIGHT']) * (newTestDF['BASELINE_WEIGHT'] - newTestDF['ACTUAL_WEIGHT'])
+	baseLineRMS = math.sqrt(np.mean(newTestDF['BASELINE_SQUARED_ERROR']))
+	print "Root Mean Squared Error for baseline model is", baseLineRMS
+
+	if type == 0:
+		newTestDF['OLS_SQUARED_ERROR'] = (newTestDF['PREDICTED_WEIGHT_OLS'] - newTestDF['ACTUAL_WEIGHT']) * (newTestDF['PREDICTED_WEIGHT_OLS'] - newTestDF['ACTUAL_WEIGHT'])
+		olsRMS = math.sqrt(np.mean(newTestDF['OLS_SQUARED_ERROR']))
+		print "Root Mean Squared Error for OLS model is", olsRMS
+	else:
+		newTestDF['RIDGE_SQUARED_ERROR'] = (newTestDF['PREDICTED_WEIGHT_RIDGE'] - newTestDF['ACTUAL_WEIGHT']) * (newTestDF['PREDICTED_WEIGHT_RIDGE'] - newTestDF['ACTUAL_WEIGHT'])
+		olsRMS = math.sqrt(np.mean(newTestDF['RIDGE_SQUARED_ERROR']))
+		print "Root Mean Squared Error for Ridge Regression model is", olsRMS
+
+	import seaborn as sns
+	sns.set_palette("hls")
+	#mpl.rc("figure", figsize=(8, 4))
+	ax1 = plt.subplot(211)
+	ax1 = sns.distplot((newTestDF['BASELINE_WEIGHT'] - newTestDF['ACTUAL_WEIGHT']), bins = 20)
+	ax1.set_xlim((-8,8))
+	ax1.set_title('Baseline Model')
+	ax2 = plt.subplot(212)
+	if type == 0:
+		ax2 = sns.distplot((newTestDF['PREDICTED_WEIGHT_OLS'] - newTestDF['ACTUAL_WEIGHT']), bins = 20)
+		ax2.set_title('OLS Model')
+	else:
+		ax2 = sns.distplot((newTestDF['PREDICTED_WEIGHT_RIDGE'] - newTestDF['ACTUAL_WEIGHT']), bins = 20)
+		ax2.set_title('Ridge Regression Model')
+	ax2.set_xlim((-8,8))
+	
+	#plt.title('Probability Distribution of Error')
+
+	plt.show()
+	
+
+print "-----------------Running OLS Regression-------------------"
+print "--------------Test Data------------------"
+runRegression(0,testDF, testWeights)
+print "--------------Evaluation Data------------------"
+runRegression(0,evalDF, evalWeights)
+print "-----------------Running Ridge Regression-------------------"
+print "--------------Test Data------------------"
+runRegression(1,testDF, testWeights)
+print "--------------Evaluation Data------------------"
+runRegression(1,evalDF, evalWeights)
+
+
+
+
---
+++ load3.py
@@ -0,0 +1,148 @@
+# coding: utf-8
+from pandas import *
+import pandas as pd
+import numpy as np
+from urllib import urlopen
+from bokeh.plotting import *
+import scipy.special
+from matplotlib import pyplot as plt    
+
+print 'Pls wait till the data loads and prints columns'
+babyCSV = urlopen("/home/alakshminara/Downloads/2008_births.csv")
+babyModCSV = urlopen("/home/alakshminara/Desktop/DataScience/WeighingBabies/birthCSVmod.csv");
+
+DF_babymod = read_csv(babyModCSV)
+DF_baby = read_csv(babyCSV)
+columns = DF_baby.columns
+print list(DF_baby.columns.values)
+DF_sammi = pd.DataFrame
+DF_train = pd.DataFrame
+DF_test = pd.DataFrame
+DF_eval = pd.DataFrame
+
+def usr_load_sammi():
+	global DF_sammi 
+	DF_sammi = pd.DataFrame(DF_baby[(DF_baby['RACEMOM']==1) & (DF_baby['RACEDAD']==1) & 
+                                (DF_baby['MAGE'] > 25)  & (DF_baby['BPOUND'] < 20) & (DF_baby['MAGE'] < 50) 
+                                & (DF_baby['SEX'] == 2)])
+	print 'Mean Birth Weight for SammiDF ',DF_sammi['BPOUND'].mean()
+	print 'Median Birth Weight for SammiDF ', DF_sammi['BPOUND'].median()	
+
+
+def usr_scatter_plot(var1,var2):
+
+       	figure(title="Dataset of Babies similar to Sammi's Baby",
+       	x_axis_label = var1,
+       	y_axis_label = var2)
+	# sample the distribution
+
+	# compute ideal values
+	#x = DF_sammi[var1]
+
+
+	# EXERCISE: output to a static HTML file
+	output_file('plot1.html')
+	# EXERCISE: turn on plot hold
+	hold()
+	
+
+	scatter(DF_sammi[var1],DF_sammi[var2], marker="square", color="black")#, title="Dataset of Babies similar to Sammi's Baby",xlabel=var1, ylabel=var2)
+
+
+	# Move the legend to a better place.
+	# Acceptable values: 'top_left', 'top_right', 'bottom_left', and 'bottom_right'
+
+	show()
+
+def usr_scatter_plot2():
+
+	# compute ideal values
+	#x = DF_sammi[var1]
+
+       	figure(title="Dataset of Babies in US",
+       	x_axis_label = 'Baby Weight (lbs.)',
+       	y_axis_label = 'Frequency')
+
+	# EXERCISE: output to a static HTML file
+	
+	# EXERCISE: turn on plot hold
+	y=[23292,31900,67140,218296,788148,1663512,1120642,280270,39109,4443,4361]
+
+	x=[2,3,4,5,6,7,8,9,10,11,12]
+
+	line(x,y, marker="square", color="black")#, title="Dataset of Babies similar to Sammi's Baby",xlabel=var1, ylabel=var2)
+
+
+	# Move the legend to a better place.
+	# Acceptable values: 'top_left', 'top_right', 'bottom_left', and 'bottom_right'
+
+	show()
+
+def usr_histogram_plot(var1):
+	hold(False)
+       	figure(title="Dataset of Babies similar to Sammi's Baby",
+       	x_axis_label = var1)
+	# sample the distribution
+
+	mu, sigma = 6.834,1      # NOTE: you can tinker with these values if you like
+
+	# sample the distribution
+	measured = np.random.normal(mu, sigma, 1000)
+	hist, edges = np.histogram(measured, density=True, bins=200)
+
+	# compute ideal values
+	x = DF_sammi[var1]
+	x = (x-min(x))/(max(x)-min(x))
+	pdf = 1/(sigma * np.sqrt(2*np.pi)) * np.exp(-(x-mu)**2 / (2*sigma**2))
+
+
+	# EXERCISE: output to a static HTML file
+	output_file('plot.html')
+	# EXERCISE: turn on plot hold
+	hold()
+
+	# Use the `quad` renderer to display the histogram bars.
+	quad(top=hist, bottom=0, left=edges[:-1], right=edges[1:],
+	     fill_color="#036565", line_color="#033649",
+
+	     # NOTE: these are only needed on the first renderer
+
+	     tools=""
+	)
+
+
+
+	# Move the legend to a better place.
+	# Acceptable values: 'top_left', 'top_right', 'bottom_left', and 'bottom_right'
+	legend().orientation = "top_left"
+
+	show()
+
+
+
+def usr_random_splitDF():
+	DF_temp = pd.DataFrame	
+	rand_nos = np.random.rand(len(DF_baby)) < 0.7	
+	DF_train = DF_baby[rand_nos]
+	DF_temp = DF_baby[~rand_nos]
+
+	rand_nos = np.random.rand(len(DF_temp)) < 0.6
+	DF_test = DF_temp[rand_nos]
+	DF_eval = DF_temp[~rand_nos] 
+
+	print 'Train(len) : {0}'.format(str(len(DF_train)))
+	print 'Test(len) : {0}'.format(str(len(DF_test)))
+	print 'Eval(len) : {0}'.format(str(len(DF_eval)))
+
+	
+def usr_print_mod_csv():
+	print DF_babymod.columns()
+	print DF_babymod.head()
+
+#usr_load_sammi()
+#usr_scatter_plot('GAINED','BPOUND')
+#usr_histogram_plot('BPOUND')
+#usr_scatter_plot2()
+#print_mean()
+#usr_random_splitDF()
+usr_print_mod_csv()
